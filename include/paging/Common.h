090c40ca9df6f8581392241b02abaa9dbc3ec06b
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-02-21 16:40:46 -0800

    feat: cmake test

--- include/paging/Common.h
+++ include/paging/Common.h
@@ -5,11 +5,10 @@
 #pragma once
 namespace paging {
 
-#define PAGE_TABLE_SIZE 1024
-#define FRAME_SIZE 128
 #define PAGE_REF_LINES 1000
 #define OUTPUT_LINE_FREQUENCY 500
 #define INPUT_FILE "input/input_page_reference.txt"
+#define TEST_INPUT_FILE "input/test_page_reference.txt"
 
 } // paging
 
---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include "paging/PagingSimulator.h"
+#include "paging/strategy/Fifo.h"
+#include "paging/strategy/Lru.h"
+#include "paging/strategy/Aging.h"
+#include "paging/strategy/Clock.h"
+#include "paging/strategy/Nru.h"
+
+
+int main() {
+  paging::PagingSimulator paging_simulator(8, 4);
+  paging_simulator.SetInput(TEST_INPUT_FILE);
+  paging_simulator.SetOutputLineFrequency(20);
+  paging_simulator.SetStrategy(std::make_unique<paging::strategy::Fifo>());
+  paging_simulator.Run();
+  paging_simulator.SetStrategy(std::make_unique<paging::strategy::Lru>());
+  paging_simulator.Run();
+  paging_simulator.SetStrategy(std::make_unique<paging::strategy::Aging>());
+  paging_simulator.Run();
+  paging_simulator.SetStrategy(std::make_unique<paging::strategy::Clock>());
+  paging_simulator.Run();
+  paging_simulator.SetStrategy(std::make_unique<paging::strategy::Nru>());
+  paging_simulator.Run();
+//  paging_simulator.SetFrameSize(8);
+//  paging_simulator.SetPageTableSize(64);
+//  paging_simulator.Run();
+  paging_simulator.ShowStats();
+  return 0;
+}
