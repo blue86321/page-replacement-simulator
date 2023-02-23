cc3f81970e8f5105c05484a8d1c68f925aef010a
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-02-22 17:48:47 -0800

    fix: Clock strategy bug (dereference, clock head_idx)

--- include/paging/strategy/Clock.h
+++ include/paging/strategy/Clock.h
@@ -9,16 +9,19 @@
 
 namespace paging::strategy {
 
+
 class Clock: public paging::IStrategy {
  public:
   uint32_t GetReplacePage(paging::PageTable &page_table) override;
-  void AfterNewPage(paging::PageTable &page_table, uint32_t page_number) override {};
-  void AfterReference(paging::PageTable &page_table, uint32_t page_number) override {};
-  void AfterReplace(paging::PageTable &page_table, uint32_t new_page_number) override {};
+  void AfterNewPage(paging::PageTable &page_table, uint32_t page_number) override;
+  void AfterReference(paging::PageTable &page_table, uint32_t page_number) override;
+  void AfterReplace(paging::PageTable &page_table, uint32_t new_page_number) override;
   std::string GetName() override;
  private:
   std::string name_ = "Clock";
   uint32_t head_idx_ = 0;
+  // element is page number
+  std::vector<uint32_t> frame_clock;
 };
 
 }
--- src/paging/strategy/Clock.cpp
+++ src/paging/strategy/Clock.cpp
@@ -7,12 +7,28 @@
 namespace paging::strategy {
 uint32_t Clock::GetReplacePage(paging::PageTable &page_table) {
   while (true) {
-    if (page_table.IsValid(head_idx_) && !page_table.IsReferenced(head_idx_)) {
-      return head_idx_;
+    uint32_t page_number = frame_clock[head_idx_];
+    if (page_table.IsValid(page_number) && !page_table.IsReferenced(page_number)) {
+      head_idx_ = (head_idx_ + 1) % frame_clock.size();
+      return page_number;
     }
-    page_table.SetReference(head_idx_, false);
-    head_idx_ = (head_idx_ + 1) % page_table.Size();
+    page_table.SetReference(page_number, false);
+    head_idx_ = (head_idx_ + 1) % frame_clock.size();
   }
+}
+void Clock::AfterNewPage(PageTable &page_table, uint32_t page_number) {
+  frame_clock.push_back(page_number);
+  AfterReference(page_table, page_number);
+}
+void Clock::AfterReference(PageTable &page_table, uint32_t page_number) {
+  for (auto pn : frame_clock) {
+    if (pn != page_number) {
+      page_table.SetReference(pn, false);
+    }
+  }
+}
+void Clock::AfterReplace(PageTable &page_table, uint32_t new_page_number) {
+  frame_clock[(head_idx_ + frame_clock.size() - 1) % frame_clock.size()] = new_page_number;
 }
 std::string Clock::GetName() {
   return name_;
