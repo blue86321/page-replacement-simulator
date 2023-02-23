2de55c213d5b5e7f744f982b5a00e57bbf5946a4
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-02-22 18:25:13 -0800

    fix: strategy Nru logic

--- include/paging/strategy/Nru.h
+++ include/paging/strategy/Nru.h
@@ -12,12 +12,14 @@ namespace paging::strategy {
 class Nru: public IStrategy {
  public:
   uint32_t GetReplacePage(PageTable &page_table) override;
-  void AfterNewPage(PageTable &page_table, uint32_t page_number) override {};
-  void AfterReference(PageTable &page_table, uint32_t page_number) override {};
-  void AfterReplace(PageTable &page_table, uint32_t new_page_number) override {};
+  void AfterNewPage(PageTable &page_table, uint32_t page_number) override;
+  void AfterReference(PageTable &page_table, uint32_t page_number) override;
+  void AfterReplace(PageTable &page_table, uint32_t old_page_number, uint32_t new_page_number) override;
   std::string GetName() override;
  private:
   std::string name_ = "NRU";
+  // element is page number
+  std::vector<uint32_t> frame_pn;
 };
 
 }
--- src/paging/strategy/Nru.cpp
+++ src/paging/strategy/Nru.cpp
@@ -6,30 +6,51 @@
 
 namespace paging::strategy {
 uint32_t Nru::GetReplacePage(PageTable &page_table) {
-  uint32_t idx = 0;
-  int64_t not_reference_modified = -1;
-  int64_t reference_not_modified = -1;
-  int64_t reference_modified = -1;
-  while (page_table.IsValid(idx) && idx < page_table.Size()) {
-    if (!page_table.IsReferenced(idx) && !page_table.IsModified(idx)) {
-      // not referenced, not modified
-      return idx;
-    } else if (!page_table.IsReferenced(idx) && page_table.IsModified(idx)) {
-      // not referenced, modified
-      not_reference_modified = idx;
-    } else if (page_table.IsReferenced(idx) && !page_table.IsModified(idx)) {
-      // referenced, not modified
-      reference_not_modified = idx;
-    } else {
-      // referenced, modified
-      reference_modified = idx;
+  int64_t default_idx = INT64_MAX;
+  int64_t not_reference_modified = default_idx;
+  int64_t reference_not_modified = default_idx;
+  int64_t reference_modified = default_idx;
+  for (auto& cur_page_no : frame_pn) {
+    if (page_table.IsValid(cur_page_no)) {
+      if (!page_table.IsReferenced(cur_page_no) && !page_table.IsModified(cur_page_no)) {
+        // not referenced, not modified
+        return cur_page_no;
+      } else if (!page_table.IsReferenced(cur_page_no) && page_table.IsModified(cur_page_no)) {
+        // not referenced, modified
+        not_reference_modified = not_reference_modified < cur_page_no ? not_reference_modified : cur_page_no;
+      } else if (page_table.IsReferenced(cur_page_no) && !page_table.IsModified(cur_page_no)) {
+        // referenced, not modified
+        reference_not_modified = reference_not_modified < cur_page_no ? reference_not_modified : cur_page_no;
+      } else {
+        // referenced, modified
+        reference_modified = reference_modified < cur_page_no ? reference_modified : cur_page_no;
+      }
     }
-    idx++;
   }
-  if (not_reference_modified != -1) return not_reference_modified;
-  if (reference_not_modified != -1) return reference_not_modified;
-  if (reference_modified != -1) return reference_modified;
-  return idx;
+  if (not_reference_modified != default_idx) return not_reference_modified;
+  if (reference_not_modified != default_idx) return reference_not_modified;
+  if (reference_modified != default_idx) return reference_modified;
+  return *frame_pn.begin();
+}
+void Nru::AfterReference(PageTable &page_table, uint32_t page_number) {
+  for (const auto& pn : frame_pn) {
+    if (pn != page_number) {
+      page_table.SetReference(pn, false);
+    }
+  }
+}
+void Nru::AfterNewPage(PageTable &page_table, uint32_t page_number) {
+  frame_pn.push_back(page_number);
+  AfterReference(page_table, page_number);
+}
+void Nru::AfterReplace(PageTable &page_table, uint32_t old_page_number, uint32_t new_page_number) {
+  for (auto& pn : frame_pn) {
+    if (pn == old_page_number) {
+      pn = new_page_number;
+    } else {
+      page_table.SetReference(pn, false);
+    }
+  }
 }
 std::string Nru::GetName() {
   return name_;
