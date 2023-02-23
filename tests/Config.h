9388431d5afd1188175f57d7e6701669644f610b
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-02-22 17:45:41 -0800

    fix: input directory

---
+++ tests/Config.h
@@ -0,0 +1,19 @@
+//
+// Created by Chunwei Chang on 2/22/23.
+//
+
+#pragma once
+
+#include <string>
+
+#define PAGE_SIZE 8
+#define FRAME_SIZE 4
+#define OUTPUT_LINE_FREQUENCY 20
+#define TEST_INPUT_FILE "input/test_page_reference.txt"
+
+namespace testing {
+
+std::string PassMsg = "Test passed";
+std::string FailMsg = "Test failed";
+
+}
