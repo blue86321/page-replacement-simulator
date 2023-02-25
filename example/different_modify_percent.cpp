fbfa922b27e6e50006139c511af526f594e5ff51
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-02-25 03:03:24 -0800

    example: different_modify_percent

--- example/different_modify_percent.cpp
+++ example/different_modify_percent.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include "paging/PagingSimulator.h"
-#include "paging/strategy/Fifo.h"
-#include "paging/strategy/Lru.h"
-#include "paging/strategy/Aging.h"
-#include "paging/strategy/Clock.h"
 #include "paging/strategy/Nru.h"
 #include <vector>
 
@@ -17,15 +13,7 @@ int main() {
     paging_simulator.SetInput(INPUT_FILE);
     paging_simulator.GenerateInputIfNotExist();
 
-//    paging_simulator.SetStrategy(std::make_unique<paging::strategy::Fifo>());
-//    paging_simulator.Run();
-//    paging_simulator.SetStrategy(std::make_unique<paging::strategy::Lru>());
-//    paging_simulator.Run();
-//    paging_simulator.SetStrategy(std::make_unique<paging::strategy::Aging>());
-//    paging_simulator.Run();
-// todo: make each file the same, only different modify pct
-    paging_simulator.SetStrategy(std::make_unique<paging::strategy::Clock>());
-    paging_simulator.Run();
+    // only NRU take modify into account
     paging_simulator.SetStrategy(std::make_unique<paging::strategy::Nru>());
     paging_simulator.Run();
     paging_simulator.ShowStats();
