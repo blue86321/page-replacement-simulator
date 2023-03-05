5661be369005511f457ede744c27de9ca1b84d21
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-03-05 13:41:17 -0800

    styles: unique_ptr move

--- example/time_algorithms.cpp
+++ example/time_algorithms.cpp
@@ -13,32 +13,24 @@
 #define PAGE 1024
 #define FRAME 128
 
-void TimeAlgo(std::string_view strategy_str) {
+void TimeAlgo(std::unique_ptr<paging::strategy::IStrategy> strategy_ptr) {
   double seconds = 0;
   uint32_t page_faults = 0;
+  std::string strategy_str = strategy_ptr->GetName();
 
+  paging::PagingSimulator paging_simulator(PAGE, FRAME);
+  paging_simulator.SetStrategyPeriod(100);
+  paging_simulator.SetInputModifyPercent(0);
+  paging_simulator.SetInput(INPUT_FILE);
+  paging_simulator.GenerateInputIfNotExist();
+  paging_simulator.SetStrategy(std::move(strategy_ptr));
   for (int i = 0; i < LOOP; i++) {
-    paging::PagingSimulator paging_simulator(PAGE, FRAME);
-    paging_simulator.SetStrategyPeriod(100);
-    paging_simulator.SetInputModifyPercent(0);
-    paging_simulator.SetInput(INPUT_FILE);
-    paging_simulator.GenerateInputIfNotExist();
-    if (strategy_str == "FIFO") {
-      paging_simulator.SetStrategy(std::make_unique<paging::strategy::Fifo>());
-    } else if (strategy_str == "LRU") {
-      paging_simulator.SetStrategy(std::make_unique<paging::strategy::Lru>());
-    } else if (strategy_str == "Aging") {
-      paging_simulator.SetStrategy(std::make_unique<paging::strategy::Aging>());
-    } else if (strategy_str == "Clock") {
-      paging_simulator.SetStrategy(std::make_unique<paging::strategy::Clock>());
-    } else if (strategy_str == "NRU") {
-      paging_simulator.SetStrategy(std::make_unique<paging::strategy::Nru>());
-    }
     paging_simulator.Run();
     auto stats = paging_simulator.GetStats();
     seconds += stats[stats.size() - 1].time_lapse.count();
     page_faults = stats[stats.size() - 1].page_fault;
   }
+
   std::cout
       << "Page: " << PAGE
       << ", Frame: " << FRAME
@@ -50,10 +42,10 @@ void TimeAlgo(std::string_view strategy_str) {
 }
 
 int main() {
-  TimeAlgo("FIFO");
-  TimeAlgo("LRU");
-  TimeAlgo("Aging");
-  TimeAlgo("Clock");
-  TimeAlgo("NRU");
+  TimeAlgo(std::move(std::make_unique<paging::strategy::Fifo>()));
+  TimeAlgo(std::move(std::make_unique<paging::strategy::Lru>()));
+  TimeAlgo(std::move(std::make_unique<paging::strategy::Aging>()));
+  TimeAlgo(std::move(std::make_unique<paging::strategy::Clock>()));
+  TimeAlgo(std::move(std::make_unique<paging::strategy::Nru>()));
   return 0;
 }
